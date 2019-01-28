4a0d032a0a4feb7aa9a9fb9afb4f596e377bfb6f
Pesticyd <44634994+Pesticyd@users.noreply.github.com>
2019-01-29 01:41:10 +0300

    test

--- sendmessage.php
+++ sendmessage.php
@@ -1,17 +1,42 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$name = htmlspecialchars($name);
-$email = htmlspecialchars($email);
-$name = urldecode($name);
-$email = urldecode($email);
-$name = trim($name);
-$email = trim($email);
-//echo $name;
-//echo "<br>";
-//echo $email;
-if (mail("mephistoeditor@yandex.ru", "Name:".$name.". E-mail: ".$email ,"From: mephistoeditor.ru \r\n"))
-{ echo "сообщение успешно отправлено";
-} else {
-  echo "при отправке сообщения возникли ошибки";	
-}?>
+$method = $_SERVER['REQUEST_METHOD'];
+//Script Foreach
+$c = true;
+if ( $method === 'POST' ) {
+	$name = trim($_POST["name"]);
+	$email  = trim($_POST["mephistoeditor@yandex.ru"]);
+	$message = trim($_POST["message"]);
+	foreach ( $_POST as $key => $value ) {
+		if ( $value != "" && $key != "name" && $key != "email" && $key != "message" ) {
+			$message .= "
+			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+			</tr>
+			";
+		}
+	}
+} else if ( $method === 'GET' ) {
+	$name = trim($_GET["name"]);
+	$email  = trim($_GET["email"]);
+	$message = trim($_GET["message"]);
+	foreach ( $_GET as $key => $value ) {
+		if ( $value != "" && $key != "name" && $key != "email" && $key != "message" ) {
+			$message .= "
+			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+			</tr>
+			";
+		}
+	}
+}
+$message = "<table style='width: 100%;'>$message</table>";
+function adopt($text) {
+	return '=?UTF-8?B?'.Base64_encode($text).'?=';
+}
+$headers = "MIME-Version: 1.0" . PHP_EOL .
+"Content-Type: text/html; charset=utf-8" . PHP_EOL .
+'From: '.adopt($name).' <'.$email.'>' . PHP_EOL .
+'Reply-To: '.$email.'' . PHP_EOL;
+mail($email, adopt($message), $message, $headers );
